0eb363aca5be871ad40e44c4fde1aef77f477db2
Jeremy Bosse <134167755+JeremyBBosse1@users.noreply.github.com>
2025-04-14 15:46:09 -0400

    Create Contact.js

---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Email: support@birthdayapp.com</p>
+    </div>
+  );
+}
+
+export default Contact;
