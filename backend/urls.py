c72974d24e3432bdd1b817e00ee663d832f251e6
Jeremy Bosse <134167755+JeremyBBosse1@users.noreply.github.com>
2025-04-14 18:09:43 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -10,4 +10,5 @@ urlpatters = [
   path('admin/', admin.site.urls),
   path('api/', include(router.urls)),
   path('api-auth/', include('rest_framework.urls')),
+  path('api/', include('app.urls')),
 ]
