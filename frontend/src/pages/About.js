0f7525f6907e8a8a6e0d4ed6debe78963822f23e
Jeremy Bosse <134167755+JeremyBBosse1@users.noreply.github.com>
2025-04-14 15:45:02 -0400

    Create About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About This App</h2>
+      <p>This app allows users to send Gmail-compatible birthday invitations.</p>
+    </div>
+  );
+}
+
+export default About;
