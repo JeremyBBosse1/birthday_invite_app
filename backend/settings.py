435397e7f2545862c7c4131228e7f246dc1c49e1
Jeremy Bosse <134167755+JeremyBBosse1@users.noreply.github.com>
2025-04-14 15:19:31 -0400

    Create settings.py

---
+++ backend/settings.py
@@ -0,0 +1,15 @@
+INSTALLED_APPS += [
+    'rest_framework',
+    'events',
+    'invitations',
+    'django_celery_results',
+]
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'django-db'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = 'your_sendgrid_api_key'
