fc11ea84de7d132e0f4121120484044d57408f0c
Jeremy Bosse <134167755+JeremyBBosse1@users.noreply.github.com>
2025-04-14 20:25:26 -0400

    Update views.py

--- backend/events/views.py
+++ backend/events/views.py
@@ -11,3 +11,25 @@ class EventViewSet(viewsets.ModelViewSet):
 
   def perform_create(self, serializer):
     serializer.save(user=self.request.user)
+    html_content = render_to_string("email_invite.html", {
+        "title": event.title,
+        "description": event.description,
+        "start_time": event.start_time,
+        "end_time": event.end_time,
+        "location": event.location,
+        "user": self.request.user,
+        "from_email": event.from_email,
+        "email_body": event.email_body,
+        "email_subject": event.email_subject,
+    })
+
+    # Send to each invitee
+    for invitee in event.invitees:
+        msg = EmailMultiAlternatives(
+            subject=event.email_subject,
+            body=event.email_body,
+            from_email=event.from_email,
+            to=[invitee]
+        )
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
