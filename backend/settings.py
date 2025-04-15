3f17ce9e360a61f49d580d9a72999b389d272a89
Jeremy Bosse <134167755+JeremyBBosse1@users.noreply.github.com>
2025-04-14 21:07:53 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -54,7 +54,7 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = 'your_sendgrid_api_key'
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
 
 DEFAULT_CHARSET = 'utf-8'
 
